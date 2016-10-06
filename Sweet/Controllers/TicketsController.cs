fe332d3ab41f62f811cdb0eadd8c859049b6480d
Henok <henok_afro@yahoo,com>
2016-10-06 03:00:48 -0400

    Add project files.

---
+++ Sweet/Controllers/TicketsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sweet.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sweet.Controllers
+{
+
+    [Authorize]
+    public class TicketsController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            ModelState.Clear();
+            return View(db.Issues.ToList());
+        }
+
+        // GET: Tickets/Details/5
+        public ActionResult Details(int? id)
+        {
+            ModelState.Clear();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Issues.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+            ModelState.Clear();
+        }
+
+        // GET: Tickets/Create
+        public ActionResult Create()
+        {
+            ModelState.Clear();
+            return View();
+        }
+
+        // POST: Tickets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TicketId,UserId,Issue,Description,Date")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Issues.Add(ticket);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+                ModelState.Clear();
+            }
+
+            return View(ticket);
+            
+        }
+
+        // GET: Tickets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Issues.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: Tickets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TicketId,UserId,Issue,Description,Date")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticket).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ticket);
+        }
+
+        // GET: Tickets/Delete/5
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Issues.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ticket ticket = db.Issues.Find(id);
+            db.Issues.Remove(ticket);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Sweet/Models/Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sweet.Models
+{
+    public class Response
+    {
+        [Key]
+        public int ResponseId { get; set; }
+
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Subject")]
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; private set; }
+
+    }
+}
